3b1beae2490ffd3c3edad586be25960ab8e67897
Sceat <sceat@aresrpg.fr>
2018-10-19 23:45:32 +0300

    temp workaround to get a common lib, refactor videoContainer

---
+++ src/lib_temp/comps/sceat-videoContainer.vue
@@ -0,0 +1,11 @@
+<template>
+  <video :poster=poster>
+    <source v-for="(source,index) in sources" :key=index :src=source.file :type=source.format>
+  </video>
+</template>
+
+<script>
+export default {
+	props: ['sources', 'poster'],
+}
+</script>
