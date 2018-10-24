e48da59955aead81b139dd179dca1cfff1a55d0d
Sceat <11330271+Sceat@users.noreply.github.com>
2018-10-24 23:27:45 +0300

    using new dummy slots

--- src/views/notYet.vue
+++ src/views/notYet.vue
@@ -1,13 +1,22 @@
 <template>
-<main>
-    <h-dummy name="under construct page content" h="600px"/>
-</main>
+	<main>
+		<h-dummy class="dummy" h="600px">
+			<h1>Under Construction</h1>
+		</h-dummy>
+	</main>
 </template>
 
 <script>
 export default {
-    	components: {
-            		hDummy: () => import('@h/hDummy.vue')
+	components: {
+		hDummy: () => import('@h/hDummy.vue'),
 	},
 }
 </script>
+
+<style lang="stylus" scoped>
+@require '~@stl/fonts'
+
+.dummy
+	font-family $sm
+</style>
