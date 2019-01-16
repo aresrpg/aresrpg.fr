5d317631344bd671fceb82263ad8ae7991a3e597
sceat <sceat@hydre.io>
2019-01-16 05:09:57 +0200

    hydre.toml

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,50 @@
 </template>
 
 <script>
-import { Vue, Component } from 'vue-property-decorator'
-import 'vue2-scrollbar/dist/style/vue2-scrollbar.css'
+	import { Vue, Component } from 'vue-property-decorator'
+	import config from '@root/hydre.toml'
 
-@Component({
-  components: {
-    utils: () => import('@cmp/utils/index.vue'),
-    scrollBar: () => import('vue2-scrollbar')
-  }
-})
-export default class App extends Vue { }
+	@Component({
+		components: {
+			utils: () => import('@cmp/utils/index.vue')
+		},
+
+		metas() {
+			const configLang = config[this.$i18n.locale]
+
+			const og = [
+				{ property: 'og:description', content: configLang.description },
+				{ property: 'og:image', content: config.ogImage }
+			]
+
+			const tw = [
+				{ property: 'twitter:description', content: configLang.description },
+				{ property: 'twitter:image', content: config.ogImage }
+			]
+
+			config.og.forEach(meta => og.push(meta))
+			config.tw.forEach(meta => tw.push(meta))
+
+			return {
+				title: configLang.title,
+				titleTemplate: eval(configLang.titleTemplate),
+				meta: [
+					{ name: 'description', content: configLang.description },
+					{ property: 'og:title', content: configLang.ogTitle, template: eval(configLang.ogTemplate) },
+					{ name: 'msapplication-TileColor', content: config.tileColor },
+					{ name: 'theme-color', content: config.themeColor },
+					...og,
+					...tw
+				],
+				link: [
+					{ rel: 'apple-touch-icon', sizes: config.touchIconSize, href: '<%= BASE_URL %>img/icons/apple-touch-icon.png' },
+					{ rel: 'mask-icon', href: '<%= BASE_URL %>img/icons/safari-pinned-tab.svg', color: config.maskIconColor }
+				]
+			}
+		}
+	})
+	export default class App extends Vue {
+	}
 </script>
 
 <style lang="stylus">
@@ -22,17 +56,28 @@ export default class App extends Vue { }
 @import url('https://fonts.googleapis.com/css?family=Assistant:200,400,600,800')
 
 :root
-  font-size 18px
+	font-size 18px
 
 *
-  sc-reset()
-  sc-disableScollBar()
-  font-family 'Assistant', sans-serif
-  outline none
-  scroll-behavior smooth
+	sc-reset()
+	font-family 'Assistant', sans-serif
+	outline none
+	scroll-behavior smooth
 
-  a
-    :active
-      color #e1c79b
-      fill #e1c79b
+	&::-webkit-scrollbar-track
+		box-shadow inset 0 0 6px rgba(0, 0, 0, .3)
+		background-color #555
+
+	&::-webkit-scrollbar
+		width 12px
+		background-color #F5F5F5
+
+	&::-webkit-scrollbar-thumb
+		box-shadow inset 0 0 6px rgba(0, 0, 0, .3)
+		background-color #252525
+
+	a
+		:active
+			color #e1c79b
+			fill #e1c79b
 </style>
